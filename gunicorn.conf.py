19d454318e1f48b4850aaa69743ae324dbb54dce
ruoxin <33121172+horryruo@users.noreply.github.com>
2022-01-07 23:37:48 +0800

    .

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-FORWARDED-PROTO': 'http', }
 loglevel = 'info'
-workers = 3
+workers = 1
 reload = True
 timeout = 60
-threads = 2
+threads = 1
